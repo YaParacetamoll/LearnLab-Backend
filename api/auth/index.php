79b4f555a2572b1b0fe704eb4bec943faf746aaa
Jarukrit <jarukritsripaploen@protonmail.com>
2024-03-02 15:15:27 +0700

    Add `GET` method to /api/auth

--- api/auth/index.php
+++ api/auth/index.php
@@ -4,20 +4,31 @@ require_once '../../initialize.php';
 
 try {
     switch ($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            if (isset($_SESSION['u_id'])) {
+                $db->where("u_id", intval($_SESSION['u_id']));
+                $user = $db->getOne("users");
+                echo json_encode(array(
+                    "status" => http_response_code(),
+                    "data" => $user
+                ));
+            } else {
+                echo jsonResponse(403, "Unauthorized");
+            }
+            break;
         case 'PUT':
             if (file_get_contents('php://input') == null) {
                 echo jsonResponse(400, "Invalid input");
             } else {
                 parse_str(file_get_contents('php://input'), $_PUT);
                 if (key_exists("u_firstname", $_PUT) && key_exists("u_lastname", $_PUT) && key_exists("u_tel", $_PUT) && key_exists("u_email", $_PUT) && key_exists("u_password", $_PUT) && key_exists("u_gender", $_PUT) && key_exists("u_role", $_PUT)) {
-                    $hash_password = password_hash($_PUT["u_hashed_password"], PASSWORD_DEFAULT);
+                    $hash_password = password_hash($_PUT["u_password"], PASSWORD_DEFAULT);
                     $data = array(
                         "u_firstname" => $_PUT["u_firstname"],
                         "u_lastname" => $_PUT["u_lastname"],
                         "u_tel" => $_PUT["u_tel"],
                         "u_email" => $_PUT["u_email"],
                         "u_hashed_password" => $hash_password,
-                        "u_access_token" => bin2hex(random_bytes(8)),
                         "u_gender" => $_PUT["u_gender"],
                         "u_role" => $_PUT["u_role"]
                     );
@@ -33,6 +44,9 @@ try {
                 $db->where("u_email", $_POST['u_email']);
                 $user = $db->getOne("users");
                 echo ($db->count > 0 && password_verify($_POST['u_password'], $user['u_hashed_password'])) ? jsonResponse(message: "Authentication success") : jsonResponse(400, "Authentication failed");
+                if (!is_null($user)) {
+                    $_SESSION['u_id'] = intval($user['u_id']);
+                }
             } else {
                 echo jsonResponse(400, "Invalid input");
             }
