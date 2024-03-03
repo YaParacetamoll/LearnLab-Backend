021cc39a9023ce185efd7e970565b8ee3c026d57
Jessada Taengsuwan <65070041@kmitl.ac.th>
2024-03-04 00:29:55 +0700

    Add user avatar selecting at when register

--- api/auth/index.php
+++ api/auth/index.php
@@ -12,46 +12,51 @@ try {
                 else if (isset($_GET['image'])) $user = $db->getOne("users", 'u_avatar, u_avatar_mime_type');
                 if (isset($user['u_avatar']) && !is_null($user['u_avatar'])) {
                     $user['u_avatar'] =  base64_encode($user['u_avatar']);
-                } 
-                echo json_encode($user
+                }
+                echo json_encode(
+                    $user
                 );
             } else {
                 echo jsonResponse(403, "Unauthorized");
             }
             break;
-        case 'PUT':
-            if (file_get_contents('php://input') == null) {
-                echo jsonResponse(400, "Invalid input");
-            } else {
-                $_PUT = json_decode(file_get_contents('php://input'), true);
-                if (key_exists("u_firstname", $_PUT) && key_exists("u_lastname", $_PUT) && key_exists("u_tel", $_PUT) && key_exists("u_email", $_PUT) && key_exists("u_password", $_PUT) && key_exists("u_gender", $_PUT) && key_exists("u_role", $_PUT)) {
-                    $hash_password = password_hash($_PUT["u_password"], PASSWORD_DEFAULT);
-                    $data = array(
-                        "u_firstname" => $_PUT["u_firstname"],
-                        "u_lastname" => $_PUT["u_lastname"],
-                        "u_tel" => $_PUT["u_tel"],
-                        "u_email" => $_PUT["u_email"],
-                        "u_hashed_password" => $hash_password,
-                        "u_gender" => $_PUT["u_gender"],
-                        "u_role" => $_PUT["u_role"]
-                    );
-                    $id = $db->insert('users', $data);
-                    if ($id) {
-                        echo jsonResponse(message: 'User was created successfully! Id = ' . $id);
-                    } else {
-                        echo jsonResponse(400, "Fail to create user.");
+        case 'POST':
+            if (isset($_POST) && key_exists("u_firstname", $_POST) && key_exists("u_lastname", $_POST) && key_exists("u_tel", $_POST) && key_exists("u_email", $_POST) && key_exists("u_password", $_POST) && key_exists("u_gender", $_POST) && key_exists("u_role", $_POST)) {
+                $hash_password = password_hash($_POST["u_password"], PASSWORD_DEFAULT);
+                $data = array(
+                    "u_firstname" => $_POST["u_firstname"],
+                    "u_lastname" => $_POST["u_lastname"],
+                    "u_tel" => $_POST["u_tel"],
+                    "u_email" => $_POST["u_email"],
+                    "u_hashed_password" => $hash_password,
+                    "u_gender" => $_POST["u_gender"],
+                    "u_role" => $_POST["u_role"]
+                );
+                if (isset($_FILES["image"]) && $_FILES["image"]["error"] == 0) {
+                    $image = $_FILES["image"]["tmp_name"];
+                    $imgContent = file_get_contents($image);
+                    $mime_type = mime_content_type($image);
+                    if (!strcmp(explode("/", $mime_type)[0], "image")) {
+                        $data["u_avatar"] = $imgContent;
+                        $data["u_avatar_mime_type"] = $mime_type;
                     }
-                } else {
-                    jsonResponse(400, "Invalid input");
                 }
+                $id = $db->insert('users', $data);
+                if ($id) {
+                    echo jsonResponse(message: 'User was created successfully! Id = ' . $id);
+                } else {
+                    echo jsonResponse(400, "Fail to create user.");
+                }
+            } else {
+                jsonResponse(400, "Invalid input");
             }
             break;
-        case 'POST':
-            $JSON_DATA = json_decode(file_get_contents('php://input'), true);
-            if (key_exists("u_email", $JSON_DATA) && key_exists("u_password", $JSON_DATA)) {
-                $db->where("u_email", $JSON_DATA['u_email']);
+        case 'PUT':
+            $_PUT = json_decode(file_get_contents('php://input'), true);
+            if (key_exists("u_email", $_PUT) && key_exists("u_password", $_PUT)) {
+                $db->where("u_email", $_PUT['u_email']);
                 $user = $db->getOne("users");
-                echo ($db->count > 0 && password_verify($JSON_DATA['u_password'], $user['u_hashed_password'])) ? jsonResponse(message: "Authentication success") : jsonResponse(400, "Authentication failed");
+                echo ($db->count > 0 && password_verify($_PUT['u_password'], $user['u_hashed_password'])) ? jsonResponse(message: "Authentication success") : jsonResponse(400, "Authentication failed");
                 if (!is_null($user)) {
                     $_SESSION['u_id'] = intval($user['u_id']);
                 }
