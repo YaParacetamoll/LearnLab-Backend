9e7cf79b83285312bda8b12800db26eaa7b0b14c
Jarukrit <jarukritsripaploen@protonmail.com>
2024-03-11 05:32:04 +0700

    Comments

--- api/message/index.php
+++ api/message/index.php
@@ -11,10 +11,11 @@ try {
             if (isset($_GET) && key_exists("c_id", $_GET) && key_exists("p_receiver", $_GET)) {
                 $db->join("messages c1", "c1.m_thread=c2.m_id", "RIGHT");
                 $db->join("posts p", "c1.p_receiver=p.p_id", "LEFT");
+                $db->join("users u", "c1.m_sender=u.u_id", "LEFT");
                 $db->where("c_id", $_GET['c_id']);
                 $db->where("c1.p_receiver", $_GET['p_receiver']);
                 $db->orderBy("c1.m_thread, c1.m_id", "asc");
-                $messages = $db->get("messages c2", null, "c1.*, p.c_id, c2.m_content AS parent_content");
+                $messages = $db->get("messages c2", null, "c1.*, p.c_id, c2.m_content AS parent_content, u.u_firstname, u.u_lastname");
                 // echo json_encode($messages);
                 if ($messages) {
                     $comments = array();
@@ -26,24 +27,35 @@ try {
                                 'm_id' => $row['m_id'],
                                 'c_id' => $row['c_id'],
                                 'm_sender' => $row['m_sender'],
+                                'u_firstname' => $row['u_firstname'],
+                                'u_lastname' => $row['u_lastname'],
                                 'p_receiver' => $row['p_receiver'],
                                 'm_content' => $row['m_content'],
+                                'created_at' => $row['created_at'],
                                 'replies' => array()
                             );
                         } else {
                             $comments[$row['m_thread']]['replies'][] = array(
                                 'm_id' => $row['m_id'],
                                 'c_id' => $row['c_id'],
+                                'u_firstname' => $row['u_firstname'],
+                                'u_lastname' => $row['u_lastname'],
                                 'm_sender' => $row['m_sender'],
                                 'm_content' => $row['m_content'],
+                                'created_at' => $row['created_at'], 
                                 'm_thread' => $row['m_thread'],
-                                'parent_content' => $row['parent_content']
                             );
                         }
                     }
-                    echo json_encode($comments);
+
+                    $data = array();
+                    foreach ($comments as $index => $val) {
+                        array_push($data, $comments[$index]);
+                    }
+
+                    echo json_encode($data);
                 } else {
-                    echo jsonResponse(message: "Post นี้ยังไม่มีการ Comment");
+                    echo json_encode(array());
                 }
             } else {
                 echo jsonResponse(400, "ค่าที่ให้มาไม่ครบหรือไม่ถูกต้อง");
@@ -81,7 +93,7 @@ try {
             $db->where("m_id", $_DELETE["m_id"]);
             $m_sender = $db->getValue("messages", "m_sender");
             $db->where("m_id", $_DELETE["m_id"]);
-            echo (!strcmp($m_sender, $_SESSION['u_id']) && $db->delete("messages")) ? jsonResponse(message: "ลบข้อความเรียบร้อย") : jsonResponse(400, "ไม่สามารถลบข้อความได้");
+            echo (!strcmp($m_sender, $_SESSION['u_id']) && $db->update("messages", array('m_content' => "ข้อความนี้ถูกลบแล้ว"))) ? jsonResponse(message: "ลบข้อความเรียบร้อย") : jsonResponse(400, "ไม่สามารถลบข้อความได้");
             break;
     }
 } catch (Exception $e) {
